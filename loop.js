3ceb16e76a05d98fdd23219ad721c545c21f39cd
smit__k_ <smitkoli2580@gmail.com>
2021-08-06 10:46:58 +0530

    javascript practice

---
+++ loop.js
@@ -0,0 +1,18 @@
+console.log("For Loop");
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+console.log("While Loop");
+let a = 5;
+while(a < 10){
+    console.log("While loop");
+    a++;
+}
+
+console.log("Do While Loop");
+let b = 0;
+do{
+    console.log("Do While loop");
+    b++;
+}while(b < 10);
---
+++ variables.js
@@ -0,0 +1,13 @@
+var a = "This is a variable";
+let b = "Scope is withing block";
+const c = 100; //This is a constant
+
+//objects
+
+var person={
+    name:"Smit",
+    age:22,
+    dob:1000,
+};
+
+console.log(a);
